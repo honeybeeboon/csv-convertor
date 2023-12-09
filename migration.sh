38f5fb07767830cf23686a3da69b08675e1df035
honeybeeboon <firstshining0119@yahoo.co.jp>
2023-12-09 13:22:42 +0900

    Create migration.sh

---
+++ migration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# CSV形式のデータをRedisにセットとして追加
+
+# CSVファイルを処理し、キーと値をRedisにセットとして追加
+cat output.csv | while IFS=, read -r key value
+do
+  # キーと値を整形
+  key=$(echo $key | sed 's/"//g')
+  value=$(echo $value | sed 's/^"//' | sed 's/"$//')  # カンマを含む文字列を取り除く
+  echo "key: $key, value: $value"
+
+  redis-cli SET $key $value
+done
